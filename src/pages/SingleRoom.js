cae7a6b3203db5e04d2be4f70cecf0736e28de7d
bonannogiovanni <bonannogiovanni@hotmail.com>
2021-04-19 23:44:08 +0200

    hero gone

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 
 import Banner from "../components/Banner";
-import Hero from "../components/Hero";
 import { Link } from "react-router-dom";
 import { RoomContext } from "../context";
 import StyledHero from "../components/StyledHero";
